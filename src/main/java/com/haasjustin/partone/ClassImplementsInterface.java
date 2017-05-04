12ba1a2307347b8d71d53af7c05413b0e9dc36f8
Justin Haas <justinmhaas1@gmail.com>
2017-05-04 19:09:47 -0400

    Testing Class Implementation

---
+++ src/main/java/com/haasjustin/partone/ClassImplementsInterface.java
@@ -0,0 +1,18 @@
+package com.haasjustin.partone;
+
+/**
+ * Created by justinhaas on 5/4/17.
+ */
+public class ClassImplementsInterface {
+
+
+    static boolean classImplementsInterface(Class implementor, Class implemented) {
+        Class[] interfaces = implementor.getInterfaces();
+        for (Class cl : interfaces) {
+
+        }
+
+    }
+
+
+}
--- src/test/java/com/haasjustin/partonetest/TestClassImplementsInterface.java
+++ src/test/java/com/haasjustin/partonetest/TestClassImplementsInterface.java
@@ -1,5 +1,6 @@
 package com.haasjustin.partonetest;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
@@ -8,4 +9,51 @@ import org.junit.Assert;
  * Created by justinhaas on 5/4/17.
  */
 public class TestClassImplementsInterface {
+
+    private Class testInterface;
+    private Class ArrayList;
+
+    @Before
+    public void setUp() {
+        try {
+            testInterface = Class.forName("java.util.List");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void testClassImplementsInterfaceTrue()
+
+    {
+        //:given
+//        implementor =
+//        implemented =
+//        expectedResult =
+
+
+        //:when
+        //actualResult =
+
+        //:then
+        //Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testClassImplementationsInterfaceFalse()
+
+    {
+        //given
+//        implementor =
+//        implemented =
+//        expectedResult =
+
+        //:when
+//        actualResult =
+
+        //:then
+        //      Assert.assertEquals(expectedResult, actualResult);
+    }
+
+
 }
