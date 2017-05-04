b27e4eadf677114a288bf5e0866b72bd5a6b55d3
Justin Haas <justinmhaas1@gmail.com>
2017-05-04 17:26:45 -0400

    Started assignment

---
+++ src/main/java/com/haasjustin/partone/classImplementsInterface.java
@@ -0,0 +1,11 @@
+package com.haasjustin.partone;
+
+/**
+ * Created by justinhaas on 5/4/17.
+ */
+public class classImplementsInterface {
+
+
+
+
+}
---
+++ src/test/java/com/haasjustin/partonetest/TestClassImplementsInterface.java
@@ -0,0 +1,11 @@
+package com.haasjustin.partonetest;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+/**
+ * Created by justinhaas on 5/4/17.
+ */
+public class TestClassImplementsInterface {
+}
